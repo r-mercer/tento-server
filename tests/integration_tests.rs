a784d0e7653162abc1e33c665793380e35c1d709
r-mercer <riley.mercer4@gmail.com>
2026-02-07 21:48:08 +0800

    the tests that pass

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -2,12 +2,7 @@ use tento_server::models::user_model::User;
 
 #[actix_web::test]
 async fn test_user_serialization_in_form() {
-    let user = User {
-        first_name: "Integration".to_string(),
-        last_name: "Test".to_string(),
-        username: "inttest".to_string(),
-        email: "integration@test.com".to_string(),
-    };
+    let user = User::new("Test", "User", "inttest", "inttest@example.com");
 
     let json_str = serde_json::to_string(&user).unwrap();
     let deserialized: User = serde_json::from_str(&json_str).unwrap();
@@ -16,7 +11,7 @@ async fn test_user_serialization_in_form() {
 }
 
 #[cfg(test)]
-mod sync_tests {
+mod tests {
     use tento_server::models::user_model::User;
 
     #[test]
