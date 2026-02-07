5ef7def9fe2d9da4db7e9a9ae79d7c314944bce7
r-mercer <riley.mercer4@gmail.com>
2026-02-07 21:21:12 +0800

    deleting db file, changing controllers to services

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, middleware::Logger, post, web};
+use mongodb::{Client, Collection, IndexModel, bson::doc, options::IndexOptions};
+mod controllers;
+mod db;
+mod models;
+use crate::controllers::user;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let uri =
+        std::env::var("MONGO_CONN_STRING").unwrap_or_else(|_| "mongodb://localhost:27017".into());
+
+    let client = Client::with_uri_str(uri).await.expect("failed to connect");
+
+    println!("starting HTTP server on port 8080");
+    println!("GraphiQL playground: http://localhost:8080/graphiql");
+    db::mongo::create_username_index(&client).await;
+
+    HttpServer::new(move || {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .app_data(web::Data::new(client.clone()))
+            .wrap(Logger::default())
+            .service(user::add_user)
+            .service(user::get_user)
+        // .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
